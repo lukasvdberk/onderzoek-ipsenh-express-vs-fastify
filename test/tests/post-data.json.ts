ea68e0afb0397f17b82bc351e217add2e0bc4163
Lukas van den Berk <lukasvdberk@gmail.com>
2022-11-25 10:54:09 +0100

    randomized post and get data

--- test/tests/post-data.json.ts
+++ test/tests/post-data.json.ts
@@ -4,12 +4,30 @@ import * as assert from "assert";
 const URL = 'http://localhost:3000'
 
 const postData = [
-    { name: 'example0', description: 'example0', price: 0 },
-    { name: 'example1', description: 'example1', price: 1 },
-    { name: 'example2', description: 'example2', price: 2 },
-    { name: 'example3', description: 'example3', price: 3 },
-    { name: 'example4', description: 'example4', price: 4 },
-    { name: 'example5', description: 'example5', price: 5 },
+    { kasdfkljasdkfl: 'asdfasd324234234', askjdfklasdjfl: 'exam223asfdafsdfasd4234234ple0', fff123123: 12312321 },
+    { asdfaasdfasdfawsfasdfsdasdfa: 'example1fadsfads', asdf312312312321: 'example1', asdfasdfasfas: 1123123123123 },
+    { asdf: 'asdfasd123123123', asdfasdf12312312: 'asdfasfasdfasfdsafadsd', dfasdfaprice: 12312312123123123123 },
+    { kasdfkljasdkfl: 'asdfasd324234234', askjdfklasdjfl: 'sfadfasd', fff123123: 12312321 },
+    { asdfaasdfasdfawsfasdfsdasdfa: 'example1', asdf312312312321: 'example1', asdfasdfasfas: 1123123123123 },
+    { asdfasdfasdfasf: 'asdfasd123123123', asdfasdf12312312: 'asdfasfasdfasd', dfasdfaprice: 112312312323123123123 },
+    { kasdfkljasdkfl: 'asdfasd324234234', askjdfklasdjfl: 'exam2234234234ple0', fff123123: 12312321 },
+    { asdfaasdfasdfawsfasdfsdasdfa: 'example1', asdf312312312321: 'example1', asdfasdfasfas: 1123123123123 },
+    { asdfasdfasdfasf: 'asdfasd123123123', asdfasdf12312312: 'asdfasfasdfasd', dfasdfaprice: 123123123123 },
+    { kasdfkljasdkfl: 'asdfasd324234234', askjdfklasdjfl: 'exam2234234234ple0', fff123123: 12312321 },
+    { asdfaasdfasdfawsfasdfsdasdfa: 'example1', asdf312312312321: 'example1', asdfasdfasfas: 1123123123123 },
+    { asdfasdfasdfasf: 'asdfasd123123123', asdfasdf12312312: 'asdfasfasdfasd', dfasdfaprice: 123123123123 },
+    { afdsadsf: 'asdfasd324234234', askjdfklasdjfl: 'exam2234234234ple0', fff123123: 12312321 },
+    { asdfaasdfasdfawsfasdfsdasdfa: 'example1', asdf3123123asdfasdf12321: 'example1', asdfasdfasfas: 1123123123123 },
+    { asdfasdfdsfaafsdfadsasdfasf: 'asdfasd123123123', asdfasdf12312312: 'asdfsadfadsfasfasdfasd', dfasdfaprice: 123123121231231233123 },
+    { kasdfkljfasdasdasdkfl: 'asdfasd324234234', askjdfklasdjfl: 'exam2234234234ple0', fff123123: 12312321 },
+    { asdfaasdfasasdfasdfdfawsfasdfsdasdfa: 'example1', asdf312312312321: 'example1', asdfasdfasfas: 1123123123123 },
+    { asdfasdfasdfasf: 'asdfasd123123123', asdfasdf12312312: 'asdfasfasdfasd', dfasdfaprice: 1231231233123123123123 },
+    { kasdfkljasdkfl: 'asdfasd324234234', askjdfklasdjfl: 'exam223asfdafsdfasd4234234ple0', fff123123: 12312321 },
+    { asdfaasdfasdfawsfasdfsdasdfa: 'example1fadsfads', asdf312312312321: 'example1', asdfasdfasfas: 1123123123123 },
+    { asdf: 'asdfasd123123123', asdfasdf12312312: 'asdfasfasdfasfdsafadsd', dfasdfaprice: 12312312123123123123 },
+    { kasdfkljasdkfl: 'asdfasd324234234', askjdfklasdjfl: 'sfadfasd', fff123123: 12312321 },
+    { asdfaasdfasdfawsfasdfsdasdfa: 'example1', asdf312312312321: 'example1', asdfasdfasfas: 1123123123123 },
+    { asdfasdfasdfasf: 'asdfasd123123123', asdfasdf12312312: 'asdfasfasdfasd', dfasdfaprice: 112312312323123123123 },
 ]
 export async function makePostRequestData() {
     // make a post request with axios
